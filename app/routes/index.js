9126400f66dfefd9f56daf394af3b40ae72cc320
nogada <seungil.han@gmail.com>
2023-09-28 10:18:40 +0900

    add add nodemon

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Welcome k8s api service");
+  res.send("Welcome k8s api service !!");
 });
 
 module.exports = router;
